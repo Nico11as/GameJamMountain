83ca6a766ffc1fd6e807674f0cb2c6ad7ba0bf93
Nico11as <mnicollasmorais@gmail.com>
2026-09-06 16:39:24 -0300

    V0.0044: Início do sistema de adjacência dos nós de mapa

--- Objects/Prefabs/MapNode.gd
+++ Objects/Prefabs/MapNode.gd
@@ -4,9 +4,14 @@ extends Button
 @onready var game_manager: GameManager = $"../.."
 @onready var dice_holder: DiceManager = $"../../DiceHolder"
 
-@export_category("Config")
+@export_subgroup("Config")
 @export var cost: int = 5
 @export var bought: bool
+
+@export_category("Nodes next to it")
+@export var next: MapNode
+@export var left: MapNode
+@export var right: MapNode
 
 func CheckBuying() -> void:
 	if dice_holder.current_value >= cost && !bought:
